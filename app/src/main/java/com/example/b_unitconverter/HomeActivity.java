6b918c96a1db4a1edb8bf6f48ab9a3a029103437
Bongani-4 <njathubongani@gmail.com>
2024-02-06 14:17:11 +0200

    layout homepage

--- app/src/main/java/com/example/b_unitconverter/HomeActivity.java
+++ app/src/main/java/com/example/b_unitconverter/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.b_unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,49 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        /*
+
+        CardView lo = findViewById(R.id.Logout);
+        CardView  animalcaretaker = findViewById(R.id.animalcaretaker);
+        CardView news = findViewById(R.id.News);
+        CardView Equipment = findViewById(R.id.Equipment);
+
+        Equipment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, Equipment.class));
+            }
+        });
+        animalcaretaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(HomeActivity.this,animalCaretaker.class));
+
+            }
+
+        });
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,NewsActivity.class));
+            }
+        });
+
+
+        logOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(HomeActivity.this, LoginActivityFirebase.class);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+
+            }
+        });*/
     }
+
+
 }
