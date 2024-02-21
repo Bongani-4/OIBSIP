46d4d512e9a4e949de969ddcbddcdf5ab34305e5
Bongani-4 <njathubongani@gmail.com>
2024-02-21 12:39:42 +0200

    add task layout button

--- app/src/main/java/com/example/todoapplication/Addtasks.java
+++ app/src/main/java/com/example/todoapplication/Addtasks.java
@@ -1,14 +1,23 @@
 package com.example.todoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Addtasks extends AppCompatActivity {
-
-    @Override
+Button btnschedule ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        btnschedule = findViewById(R.id.Set);
+
+        btnschedule.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black));
+
+
+
+
     }
 }
