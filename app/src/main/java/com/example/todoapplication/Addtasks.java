70b1b35bb0eae8bd79e1e8dd3cab3de273c8b922
Bongani-4 <njathubongani@gmail.com>
2024-02-21 13:09:52 +0200

    add task layout button logic

--- app/src/main/java/com/example/todoapplication/Addtasks.java
+++ app/src/main/java/com/example/todoapplication/Addtasks.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class Addtasks extends AppCompatActivity {
-Button btnschedule ;
+Button btnschedule,btncancel ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
         btnschedule = findViewById(R.id.Set);
+        btncancel = findViewById(R.id.cancel);
 
         btnschedule.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black));
+        btncancel.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black));
 
 
 
